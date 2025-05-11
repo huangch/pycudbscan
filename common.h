1bc267667a79f8d79a36f792d272e133f387c704
HUANG, C.-H. <2898938+huangch@users.noreply.github.com>
2025-05-10 23:23:31 -0700

    Added DClust+

---
+++ common.h
@@ -0,0 +1,49 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+using namespace std;
+
+#define RANGE 2
+#define UNPROCESSED -1
+#define NOISE -2
+
+// #define DIMENSION 2
+// #define TREE_LEVELS (DIMENSION + 1)
+
+#define THREAD_BLOCKS 8
+#define THREAD_COUNT 16
+
+#define MAX_SEEDS 128
+#define EXTRA_COLLISION_SIZE 256
+
+// #define DATASET_COUNT 9
+
+// #define MINPTS 3
+// #define EPS 1.5
+
+#define PARTITION_SIZE 3
+#define POINTS_SEARCHED 9
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#define gpuErrchk(ans) \
+  { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char* file, int line,
+                      bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort) exit(code);
+  }
+}
+
+struct __align__(8) IndexStructure {
+  int dimension;
+  int dataBegin;
+  int dataEnd;
+  int childFrom;
+};
+
+#endif
